0d569d98416a41c7f4b054afc2a6ed450c9040ae
ZhengshiZhao <zhao.zhengshi@gmail.com>
2017-04-15 22:51:48 -0400

    add svm predict

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
-
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+c = 10000.
+my_kernel = 'rbf'
+#clf = svm.SVC(kernel='linear')
+def my_predic(c,my_kernel):
+	time_start = time()
+	clf = svm.SVC(C=c,kernel=my_kernel)
+	clf.fit(features_train,labels_train)
+	print clf
+	predict_test = clf.predict(features_test)
+	time_end = time()
+	print "predict_test: ",np.sum(predict_test)
+#	print "c = {0} accuracy_score: {1} ".format(c, accuracy_score(labels_test,predict_test))
+#	print "time: ", time_end - time_start
+my_predic(c,my_kernel)
